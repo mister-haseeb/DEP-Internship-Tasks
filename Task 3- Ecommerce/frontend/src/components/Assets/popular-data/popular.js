4371bd0c41aa8fbffb26c08ecbfb4fde3b81bed5
Muhammad Haseeb Ahmad <rajputhasib63@gmail.com>
2024-07-31 13:30:22 +0500

    Add files via upload

---
+++ Task 3- Ecommerce/frontend/src/components/Assets/popular-data/popular.js
@@ -0,0 +1,37 @@
+import img1 from "../popular-data/fashion.png";
+import img2 from "../popular-data/new.png";
+import img3 from "../popular-data/pink.png";
+import img4 from "../popular-data/upper.png";
+
+let popular_products = [
+    {
+        id: 1,
+        name: "Elegant Essentials",
+        image: img1,
+        new_price: 29.99,
+        old_price: 50.00,
+    },
+    {
+        id: 2,
+        name: "Bold Accessories",
+        image: img2,
+        new_price: 39.99,
+        old_price: 80.00,
+    },
+    {
+        id: 3,
+        name: "Pink Perfection",
+        image: img3,
+        new_price: 44.99,
+        old_price: 60.00,
+    },
+    {
+        id: 4,
+        name: "Cute Casuals",
+        image: img4,
+        new_price: 49.99,
+        old_price: 90.00,
+    },
+];
+
+export default popular_products;
---
+++ Task 3- Ecommerce/frontend/src/components/Assets/products.js
@@ -0,0 +1,29 @@
+import img1 from '../Assets/popular-data/fashion.png';
+import img2 from '../Assets/popular-data/new.png';
+import img3 from '../Assets/popular-data/pink.png';
+import img4 from '../Assets/popular-data/upper.png';
+import img5 from '../Assets/popular-data/t-shirt1.webp';
+import img6 from '../Assets/popular-data/t-shirt2.webp';
+import img7 from '../Assets/popular-data/t-shirt3.webp';
+import img8 from '../Assets/popular-data/t-shirt4.webp';
+import img9 from '../Assets/popular-data/t-shirt5.webp';
+import img10 from '../Assets/popular-data/t-shirt6.webp';
+import img11 from '../Assets/popular-data/t-shirt7.webp';
+import img12 from '../Assets/popular-data/t-shirt8.webp';
+
+const all_products = [
+    { id: 1, name: "Elegant Essentials", category: "women", image: img1, new_price: 29.99, old_price: 50.00 },
+    { id: 2, name: "Bold Accessories", category: "women", image: img2, new_price: 39.99, old_price: 80.00 },
+    { id: 3, name: "Pink Perfection", category: "women", image: img3, new_price: 44.99, old_price: 60.00 },
+    { id: 4, name: "Cute Casuals", category: "women", image: img4, new_price: 49.99, old_price: 90.00 },
+    { id: 5, name: "V-NECK T-SHIRT", category: "men", image: img5, new_price: 29.99, old_price: 50.00 },
+    { id: 6, name: "V-NECK T-SHIRT", category: "men", image: img6, new_price: 39.99, old_price: 80.00 },
+    { id: 7, name: "CREW NECK T-SHIRT", category: "men", image: img7, new_price: 39.99, old_price: 80.00 },
+    { id: 8, name: "LYCRA T-SHIRT", category: "men", image: img8, new_price: 39.99, old_price: 80.00 },
+    { id: 9, name: "Orange Dress", category: "kids", image: img9, new_price: 44.99, old_price: 60.00 },
+    { id: 10, name: "Yellow T-Shirt", category: "kids", image: img10, new_price: 44.99, old_price: 60.00 },
+    { id: 11, name: "Lime Yellow T-Shirt", category: "kids", image: img11, new_price: 44.99, old_price: 60.00 },
+    { id: 12, name: "Black T-Shirt", category: "kids", image: img12, new_price: 49.99, old_price: 90.00 },
+];
+
+export default all_products;
